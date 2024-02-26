9ebbd368f85be7facf18e89bfb5365d1b92ca957
shubratoband <159985104+shubratoband@users.noreply.github.com>
2024-02-26 10:55:44 +0000

    Conversion And Datatypes

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,10 +1,35 @@
-let score = "3"
+let score = "Shubrato"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let ValueInNumber = Number(score)
 
-console.log(typeof ValueInNumber);
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
 
 
 
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// null => NaN
+// undefined => NaN
+// String => NaN
+
+let IsLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+//1 => true ; 0 => false
+//EmptyString => false
+//String => true
+//Undefined => false
+//null => false
+
+let OneNumber = 21
+
+let toString = String(OneNumber)
+
+console.log( typeof toString);
