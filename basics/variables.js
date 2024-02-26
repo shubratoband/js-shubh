42fa692039dd86cfec1e1f394592c77d39f7870d
shubratoband <159985104+shubratoband@users.noreply.github.com>
2024-02-26 08:55:14 +0000

    Done

---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 12212
+let accountEmail = "shubratoband01@gmail.com"
+var accountPass = "234"
+accountCity = "Akola"
+let accountState
+
+// accountId = 2 // not allowed
+
+accountEmail = "sb@gmail.com"
+accountPass = "332324"
+accountCity = "Nagpur"
+
+console.log(accountId); 
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
+
+/*
+Prefer not to use var
+*/
