1e0c09cbb8b9ae0a7f6a8ef06bf469db0c9839e8
shubratoband <159985104+shubratoband@users.noreply.github.com>
2024-02-26 09:41:28 +0000

    Done

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"
+
+// alert(3 + 3) 
+
+console.log(3 + 3)
+
+console.log("Shubrato")
+
+let name = "Shubrato"
+let age = 21
+let IsLoggedIn = true
+let state = null
+
+
+//number => 2 to power 53
+//bigint
+//string
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof null);
