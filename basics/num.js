2432acb7a88de8b63082d9a6c4604342d572c6a1
shubratoband <shubratoband12@gmail.com>
2024-03-17 19:59:07 +0530

    number

---
+++ basics/num.js
@@ -0,0 +1,38 @@
+// const score = 400;
+
+// console.log(score);
+
+// const balance = new Number(400);
+// console.log( balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));            //use to decide no. of zeros after decimal
+
+// const num2 = 222.533
+// console.log(num2.toPrecision(3));           //to show the numbers after the decimal point also but give complex 
+//                                             //on putting digit that is less than decimal point 
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//    *************************************Maths**********************************
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+
+// console.log(Math.ceil(2.1));
+
+// console.log(Math.floor(2.9));
+
+// console.log(Math.min(12,6,4,5,6));
+// console.log(Math.max(12,6,4,5,6));
+
+console.log(Math.round(34.5));
+
+const num1 = 10
+const num3 = 20
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+console.log(Math.floor(Math.random())*(num3-num1)  )
